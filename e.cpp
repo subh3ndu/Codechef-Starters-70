1b6d115e20f267468da44c2f53751fb1da25a1e1
Venom-61 <subhendukarmakar61@gmail.com>
2022-12-21 21:22:37 +0530

    Minimum distants between 1s

---
+++ e.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+// E. Minimum distance between 1s
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    for(int i=0; i<n; i++) {
+             
+    }
+
+    return ;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+    for(int i=1; i<=test; i++) {
+        solve();
+    }
+
+    return 0;
+}
